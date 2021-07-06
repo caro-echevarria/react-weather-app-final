2a0c8d8bc798ef7aeb8b2e1d491f21713fccde17
unknown <caro.echevarria1991@gmail.com>
2021-07-06 16:52:55 -0500

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,40 +3,42 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        <small>
-          Coded by{" "}
-          <a
-            href="https://www.linkedin.com/in/carolina-genoveva-echevarría-aguilar"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Carolina Echevarria <i className="fab fa-linkedin"></i>
-          </a>
-          <br />
-          <a
-            href="https://github.com/caro-echevarria/react-weather-app-final"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Open-sourced <i className="fab fa-github"></i>
-          </a>{" "}
-          and{" "}
-          <a
-            href="https://competent-brown-05cd48.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            hosted at Netlify
-            <span
-              className="iconify"
-              data-icon="simple-icons:netlify"
-              data-inline="false"
-            ></span>
-          </a>
-        </small>
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+        <footer>
+          <small>
+            Coded by{" "}
+            <a
+              href="https://www.linkedin.com/in/carolina-genoveva-echevarría-aguilar"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Carolina Echevarria <i className="fab fa-linkedin"></i>
+            </a>
+            <br />
+            <a
+              href="https://github.com/caro-echevarria/react-weather-app-final"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Open-sourced <i className="fab fa-github"></i>
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://competent-brown-05cd48.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              hosted at Netlify{" "}
+              <span
+                className="iconify"
+                data-icon="simple-icons:netlify"
+                data-inline="false"
+              ></span>
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
