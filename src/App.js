4d567bbe0ca11eb6801cf4baa35eb1044a55395e
unknown <caro.echevarria1991@gmail.com>
2021-07-06 15:22:17 -0500

    Added repo url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>
+        <small>
+          Coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/carolina-genoveva-echevarría-aguilar"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Carolina Echevarria <i className="fab fa-linkedin"></i>
+          </a>
+          <br />
+          <a
+            href="https://github.com/caro-echevarria/weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-sourced <i className="fab fa-github"></i>
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://competent-brown-05cd48.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted at Netlify
+            <span
+              className="iconify"
+              data-icon="simple-icons:netlify"
+              data-inline="false"
+            ></span>
+          </a>
+        </small>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
